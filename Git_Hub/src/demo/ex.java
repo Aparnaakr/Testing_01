afebdc535096b8ee27abdfde5bc3ee0142eb3134
Admin <Admin@DESKTOP-804836M>
2024-12-12 16:11:04 +0530

    by xyz

--- Git_Hub/src/demo/ex.java
+++ Git_Hub/src/demo/ex.java
@@ -1,6 +1,18 @@
 package demo;
 
 public class ex {
+	
+	public void m1()
+	{
+		System.out.println("m1 method.......");
+	}
+	
+	public void m2()
+	{
+		System.out.println("m2 method.......");
+	}
+	
+	
 	public static void main(String[] args) {
 		System.out.println("hello");
 	}
